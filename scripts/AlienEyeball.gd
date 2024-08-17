400c9f44f6dfdbaacd156b67d9bd1a950cefc649
Joshua <Joshreyes@gmail.com>
2024-08-17 23:48:49 +0100

    added alot but fixed the test world

---
+++ scripts/AlienEyeball.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+@export var eye_radius: float = 5
+
+var eye_center: Vector2
+
+func _ready():
+	eye_center = position
+
+func _process(delta):
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		var distance_to_player = global_position.distance_to(player.global_position)
+		var clamped_distance = min(distance_to_player, eye_radius)
+		
+		position = eye_center + direction * clamped_distance
--- scripts/alien.gd
+++ scripts/alien.gd
@@ -4,5 +4,25 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 var FOLLOW_SPEED = 0.5
 
+@export var isplaying = false
+@export var flipsprite = false
+
+func _ready():
+	if flipsprite:
+		$body.flip_h = true
+		$turret.position = Vector2(546, 103)
+		$turret2.position = Vector2(374, 53)
+		
+		$AlienEyeball.position = Vector2(227, 251)
+		$AlienEyeball.eye_center = Vector2(227, 251)
+		$AlienEyeball2.position = Vector2(337, 257)
+		$AlienEyeball2.eye_center = Vector2(337, 257)
+		
+		$Alieneyes.flip_h = true
+
 func _process(delta):
-	position.x = lerp(position.x,player.position.x-100,delta*FOLLOW_SPEED)
+	if not isplaying:
+		position.x = lerp(position.x,player.position.x-100,delta*FOLLOW_SPEED)
+		Global.ceasefire = false
+	if isplaying:
+		Global.ceasefire = true
