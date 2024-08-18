12a2929bbf023cb33b079b332cc51aaf1fff9048
Joshua <Joshreyes@gmail.com>
2024-08-18 21:48:02 +0100

    aagain

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 func _on_start_pressed():
 	$click.play()
 	await get_tree().create_timer(0.1).timeout
-	get_tree().change_scene_to_file("res://levels/climbtest.tscn")
+	get_tree().change_scene_to_file("res://levels/actual levels/Level1.tscn")
 
 func _on_exit_pressed():
 	$click.play()
