a1cfafff44aefab8af10af9049eb1cc5a8b15798
Joshua <Joshreyes@gmail.com>
2024-08-19 19:28:33 +0100

    minor tweaks

--- scripts/shelf player.gd
+++ scripts/shelf player.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var player = get_tree().get_first_node_in_group("player")
 var t = 0
 var t2  = 0
+var t3 = 0
 
 func _on_zomout_body_entered(body):
 	if body == player:
@@ -21,5 +22,12 @@ func _on_scene_change_body_entered(body):
 			var tween = create_tween() 
 			tween.set_parallel()
 			tween.tween_property($"../player/Camera2D","zoom",Vector2(1,1),2)
-			tween.tween_property($"../alien","position",Vector2(468, 1788),5)
+			tween.tween_property($"../alien","position",Vector2(30659, -1579),5)
 			$"../alien".isplaying = true
+
+
+func _on_zomout_2_body_entered(body):
+		if body == player:
+			t3 += 1
+			if t3 == 1:
+				$"../alien".shelving = false
